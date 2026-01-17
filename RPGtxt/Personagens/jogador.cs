bc1ed2b46849c2dcf049af3bb0d27e7a38451c86
Jonnyh Lima <jonhsakamaki1999@gmail.com>
2026-01-16 21:04:06 -0300

    feat: :sparkles: Jogador
    Status para o Jogador finalizado!

--- RPGtxt/Personagens/jogador.cs
+++ RPGtxt/Personagens/jogador.cs
@@ -1,9 +1,48 @@
 namespace RPGtxt.Personagens;
 
-public class Jogador
+public class Jogador : PersonagemBase
 {
-    public string Nome {get; set;} = null!;
-    public int Vida {get; set;}
-    public int Ataque {get; set;}
+    public Jogador(string nome) : base(nome,150, 10, 20, 10)
+    {
+        Nivel = 1;
+        ExpAtual = 0;
+        ExpNecessaria = 100;
+    }
+    public int Nivel {get; private set;}
+    public int ExpAtual{get; private set;}
+    public int ExpNecessaria{get; private set;}
+
+    public void GanharExp(int quantidade)
+    {
+        ExpAtual += quantidade;
+        Console.WriteLine($"{Nome} ganhou {quantidade} de Experiencia! Falta {ExpNecessaria} para subir de nivel!");
+
+        while(ExpAtual >= ExpNecessaria)
+        {
+            SubirDeNivel();
+        }
+    }
+
+    private void SubirDeNivel()
+    {
+        ExpAtual -= ExpNecessaria;
+        Nivel++;
+
+        VidaMaxima += 25;
+        VidaAtual = VidaMaxima;
+        AtkMin += 3;
+        AtkMax += 5;
+        Defesa += 3;
+
+
+        ExpNecessaria = (int)(ExpNecessaria * 1.7);
+
+        Console.WriteLine($"****LEVEL UP!****");
+        Console.WriteLine($"{Nome} agora esta no Nivel {Nivel}!");
+        Console.WriteLine($"Status aumentados! \n Vida: {VidaMaxima} \n Ataque: {AtkMin}~{AtkMax} \n Defesa: {Defesa}");
+    }
+
 }
+
+  
 
